ee442cb91f8c064467340fdb51a77754177e6688
sgrant0225 <sharnellgrant@gmail.com>
2020-05-25 22:11:57 -0400

    created signup routes, started login routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,34 @@
 class UsersController < ApplicationController
-
+ 
 #users signup informtion will be stored here
 
- get '/users/new' do
+ get '/signup' do
  
-  erb :'/users/new'
+  erb :'/signup'
  end 
 
- post '/users' do
-   binding.pry
-   new_user = User.new(params)
-  #  if new_user.save 
-  #  sessions[:user_id] = new_user.id 
- end   
+ post '/signup' do
+   @new_user = User.new(params)
+    if @new_user.save 
+    session[:user_id] = @new_user.id 
+    binding.pry
+    redirect '/login'
+    #instead of login page redirect to a show page
+    else
+      erb :'/signup'
+  end
+ end
+ 
+ #get user login and show login form for existing users. Need get request and login erb file
+ #post request 1.Find the user #authenticate the user Redirect to the users landing show page
+
+  get '/login' do
+   "THIS IS THE LOGIN PAGE"
+   erb :'/login'
+  end
+
+  # post 'login'
+
+  # end 
+ 
 end    
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base 
   has_secure_password
-    
+  validates :email, :password, presence: true  
   has_many :post_discussions
 end
